9273c60c190d7081abb8f2ec59f309c892cc56a2
sunghee <h33uid@gmail.com>
2025-03-23 18:19:24 +0900

    chore(wezterm): update format

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,22 @@
-local wezterm = require 'wezterm'
-local act = wezterm.action
-local config = {}
+local wezterm           = require 'wezterm'
+local act               = wezterm.action
+local config            = {}
 
-config = wezterm.config_builder()
+config                  = wezterm.config_builder()
 
-config.front_end = "OpenGL"
-config.animation_fps = 60
+config.front_end        = "OpenGL"
+config.animation_fps    = 60
 
-config.default_prog = { "C:\\Program Files\\PowerShell-7.3.5-win-x64\\pwsh.exe" }
---config.default_prog = { "/bin/bash" }
+-- config.default_prog = { "C:\\Program Files\\PowerShell-7.3.5-win-x64\\pwsh.exe" }
+-- config.default_prog = { "/bin/bash" }
 
-local custom_catppuccin      = wezterm.color.get_builtin_schemes()["Catppuccin Latte"]
---local custom_catppuccin      = wezterm.color.get_builtin_schemes()["Catppuccin Macchiato"]
+local custom_catppuccin = wezterm.color.get_builtin_schemes()["Catppuccin Latte"]
+-- local custom_catppuccin      = wezterm.color.get_builtin_schemes()["Catppuccin Macchiato"]
 -- custom_catppuccin.background = '#293643'
-config.color_schemes         = { ['Custom_Catpuccin'] = custom_catppuccin }
-config.color_scheme          = 'Custom_Catpuccin'
+config.color_schemes    = { ['Custom_Catpuccin'] = custom_catppuccin }
+config.color_scheme     = 'Custom_Catpuccin'
 
-config.font = wezterm.font(
+config.font             = wezterm.font(
   'Hack Nerd Font',
   {
     weight = 'Regular',
@@ -24,60 +24,60 @@ config.font = wezterm.font(
   }
 )
 
-config.window_padding = {
+config.window_padding   = {
   left = 1, right = 1, top = 1, bottom = 1
 }
 
-config.leader = { key = "e", mods = "ALT" }
+config.leader           = { key = "e", mods = "ALT" }
 
-config.keys = {
+config.keys             = {
   -- PANE
   -- create & delete
-  { key = '_', mods = 'ALT|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' }, },
-  { key = "|", mods = "ALT|SHIFT", action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
-  { key = "q", mods = "ALT", action = act.CloseCurrentPane { confirm = false }, },
+  { key = '_',          mods = 'ALT|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' }, },
+  { key = "|",          mods = "ALT|SHIFT", action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+  { key = "q",          mods = "ALT",       action = act.CloseCurrentPane { confirm = false }, },
   -- navigate among panes
-  { key = 'h', mods = 'ALT', action = act.ActivatePaneDirection 'Left', },
-  { key = 'LeftArrow', mods = 'ALT', action = act.ActivatePaneDirection 'Left', },
-  { key = 'j', mods = 'ALT', action = act.ActivatePaneDirection 'Down', },
-  { key = 'DownArrow', mods = 'ALT', action = act.ActivatePaneDirection 'Down', },
-  { key = 'k', mods = 'ALT', action = act.ActivatePaneDirection 'Up', },
-  { key = 'UpArrow', mods = 'ALT', action = act.ActivatePaneDirection 'Up', },
-  { key = 'l', mods = 'ALT', action = act.ActivatePaneDirection 'Right', },
-  { key = 'RightArrow', mods = 'ALT', action = act.ActivatePaneDirection 'Right', },
+  { key = 'h',          mods = 'ALT',       action = act.ActivatePaneDirection 'Left', },
+  { key = 'LeftArrow',  mods = 'ALT',       action = act.ActivatePaneDirection 'Left', },
+  { key = 'j',          mods = 'ALT',       action = act.ActivatePaneDirection 'Down', },
+  { key = 'DownArrow',  mods = 'ALT',       action = act.ActivatePaneDirection 'Down', },
+  { key = 'k',          mods = 'ALT',       action = act.ActivatePaneDirection 'Up', },
+  { key = 'UpArrow',    mods = 'ALT',       action = act.ActivatePaneDirection 'Up', },
+  { key = 'l',          mods = 'ALT',       action = act.ActivatePaneDirection 'Right', },
+  { key = 'RightArrow', mods = 'ALT',       action = act.ActivatePaneDirection 'Right', },
   -- resize pane
-  { key = 'h', mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Left', 5 }, },
-  { key = 'LeftArrow', mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Left', 5 }, },
-  { key = 'j', mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Down', 5 }, },
-  { key = 'DownArrow', mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Down', 5 }, },
-  { key = 'k', mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Up', 5 }, },
-  { key = 'UpArrow', mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Up', 5 }, },
-  { key = 'l', mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Right', 5 }, },
+  { key = 'h',          mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Left', 5 }, },
+  { key = 'LeftArrow',  mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Left', 5 }, },
+  { key = 'j',          mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Down', 5 }, },
+  { key = 'DownArrow',  mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Down', 5 }, },
+  { key = 'k',          mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Up', 5 }, },
+  { key = 'UpArrow',    mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Up', 5 }, },
+  { key = 'l',          mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Right', 5 }, },
   { key = 'RightArrow', mods = 'ALT|SHIFT', action = act.AdjustPaneSize { 'Right', 5 }, },
   -- swap pane
-  { key = 's', mods = 'ALT', action = act.PaneSelect { mode = 'SwapWithActive' } },
+  { key = 's',          mods = 'ALT',       action = act.PaneSelect { mode = 'SwapWithActive' } },
 
   -- WINDOW & TAB
-  { key = "c", mods = "ALT", action = act.SpawnTab 'CurrentPaneDomain', },
-  { key = "t", mods = "ALT", action = act.SpawnWindow, },
-  { key = "x", mods = "ALT", action = act.CloseCurrentTab { confirm = false }, },
-  { key = "n", mods = "ALT", action = act.ActivateTabRelative(1), },
-  { key = "p", mods = "ALT", action = act.ActivateTabRelative(-1), },
+  { key = "c",          mods = "ALT",       action = act.SpawnTab 'CurrentPaneDomain', },
+  { key = "t",          mods = "ALT",       action = act.SpawnWindow, },
+  { key = "x",          mods = "ALT",       action = act.CloseCurrentTab { confirm = false }, },
+  { key = "n",          mods = "ALT",       action = act.ActivateTabRelative(1), },
+  { key = "p",          mods = "ALT",       action = act.ActivateTabRelative(-1), },
 
   -- SCROLL
-  { key = 'u', mods = 'ALT', action = act.ScrollByLine(-5) },
-  { key = 'd', mods = 'ALT', action = act.ScrollByLine(5) },
-  { key = 'u', mods = 'ALT|SHIFT', action = act.ScrollByPage(-0.5) },
-  { key = 'd', mods = 'ALT|SHIFT', action = act.ScrollByPage(0.5) },
-  { key = 'u', mods = 'CTRL|ALT', action = act.ScrollToTop },
-  { key = 'd', mods = 'CTRL|ALT', action = act.ScrollToBottom },
+  { key = 'u',          mods = 'ALT',       action = act.ScrollByLine(-5) },
+  { key = 'd',          mods = 'ALT',       action = act.ScrollByLine(5) },
+  { key = 'u',          mods = 'ALT|SHIFT', action = act.ScrollByPage(-0.5) },
+  { key = 'd',          mods = 'ALT|SHIFT', action = act.ScrollByPage(0.5) },
+  { key = 'u',          mods = 'CTRL|ALT',  action = act.ScrollToTop },
+  { key = 'd',          mods = 'CTRL|ALT',  action = act.ScrollToBottom },
 
   -- ETC
-  { key = "c", mods = "CTRL|ALT", action = act.ActivateCopyMode, },
-  { key = "f", mods = "ALT", action = act.Search('CurrentSelectionOrEmptyString'), },
-  { key = "z", mods = "ALT", action = act.TogglePaneZoomState, },
-  { key = "z", mods = "CTRL|ALT", action = act.ToggleFullScreen, },
-}  -- configs.keys
+  { key = "c",          mods = "CTRL|ALT",  action = act.ActivateCopyMode, },
+  { key = "f",          mods = "ALT",       action = act.Search('CurrentSelectionOrEmptyString'), },
+  { key = "z",          mods = "ALT",       action = act.TogglePaneZoomState, },
+  { key = "z",          mods = "CTRL|ALT",  action = act.ToggleFullScreen, },
+} -- configs.keys
 
 -- TAB
 for i = 1, 9 do
